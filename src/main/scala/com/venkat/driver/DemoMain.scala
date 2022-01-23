7dd6ba0f663c7cfd18219a38a04b5d3e71178dfe
rajasurender11 <rajasurender11@gmail.com>
2022-01-23 18:18:23 +0530

    methods added

--- src/main/scala/com/venkat/driver/DemoMain.scala
+++ src/main/scala/com/venkat/driver/DemoMain.scala
@@ -1,7 +1,7 @@
 package com.venkat.driver
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions.{col, lit, lower}
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 object DemoMain {
@@ -44,6 +44,21 @@ object DemoMain {
 
     accountsDF.createOrReplaceTempView("accounts_table")
     spark.sql("""select  cust_name, bank_name, ph_no from  accounts_table""").show(10,false)
+
+    accountsDF.filter(col("bank_name") === "HDFC" or col("bank_name") === "SBI" ).show(10,false)
+    spark.sql("""select * from accounts_table where  bank_name = 'HDFC' """).show(10,false)
+
+    spark.sql("""select  account_no, bank_name, cust_name, gender, ph_no as mobile_num from accounts_table  """).show(100,false)
+    val updatedDF =  accountsDF.withColumnRenamed("ph_no" ,"mobile_number").drop("account_no")
+    updatedDF.show(10)
+
+    val addedDF = accountsDF.withColumn("mobile_number",col("ph_no")).withColumn("account_type",lit("savings")).withColumn("bank_name",lower(col("bank_name")))
+
+    val df1 = accountsDF
+    val df2 = accountsDF
+
+    df1.union(df2).distinct().show(100,false)
+
   }
 
 }
