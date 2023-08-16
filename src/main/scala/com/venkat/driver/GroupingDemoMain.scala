d963b8f0a6988c2c800118e0246fa97f447e8551
rajasurender11 <rajasurender11@gmail.com>
2023-08-16 07:32:09 +0530

    commit property

---
+++ src/main/scala/com/venkat/driver/GroupingDemoMain.scala
@@ -0,0 +1,12 @@
+package com.venkat.driver
+
+import com.venkat.process.GroupingDemo
+
+object GroupingDemoMain {
+
+  def main(args: Array[String]): Unit = {
+    val obj = new GroupingDemo()
+    obj.groupingDemo()
+  }
+
+}
